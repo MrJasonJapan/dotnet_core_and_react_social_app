e67f13a7aed60013b84d3ef98e92a6229650970d
SpaGettys <jgettys@gmail.com>
2022-05-02 15:55:49 +0900

    Adjust program.cs to create the DB if it doesn't exist yet, and apply any existing migrations.
    Newly added Migration:
    dotnet ef migrations add InitialCreate -p Persistence/ -s API/

--- Reactivities/API/Program.cs
+++ Reactivities/API/Program.cs
@@ -1,10 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
 namespace API
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using var scope = host.Services.CreateScope();
+
+            var services = scope.ServiceProvider;
+
+            try
+            {
+                var context = services.GetRequiredService<DataContext>();
+                // Database will be created here if it does not exist yet, and then all migrations will be applied.
+                context.Database.Migrate();
+            }
+            catch (Exception ex)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An error occured during migration.");
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
