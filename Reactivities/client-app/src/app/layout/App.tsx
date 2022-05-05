456e6b43a3856acb660421c75c44f51475a65970
SpaGettys <jgettys@gmail.com>
2022-05-05 20:25:29 +0900

    Add logic to select a specific Activity from the list to show on the right side of the dashboard.
    We will need to setup state inside App.tsx, and send its Set-State function down to the Dashboard -> List. The Selected state will then be passed down to the Dashboard -> Activity Details.

--- Reactivities/client-app/src/app/layout/App.tsx
+++ Reactivities/client-app/src/app/layout/App.tsx
@@ -4,9 +4,11 @@ import { Container, List } from 'semantic-ui-react';
 import { Activity } from './../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+import { act } from 'react-dom/test-utils';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
 
   useEffect(() => {
     axios.get<Activity[]>('http://localhost:5000/api/activities').then((response) => {
@@ -14,11 +16,24 @@ function App() {
     });
   }, []);
 
+  function handleSelectActivity(id: string) {
+    setSelectedActivity(activities.find((x) => x.id === id));
+  }
+
+  function handleCancelSelectActivity() {
+    setSelectedActivity(undefined);
+  }
+
   return (
     <>
       <NavBar />
       <Container style={{ marginTop: '7em' }}>
-        <ActivityDashboard activities={activities} />
+        <ActivityDashboard
+          activities={activities}
+          selectedActivity={selectedActivity}
+          selectActivity={handleSelectActivity}
+          cancelSelectActivity={handleCancelSelectActivity}
+        />
       </Container>
     </>
   );
