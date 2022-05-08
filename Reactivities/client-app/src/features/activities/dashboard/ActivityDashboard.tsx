2de7b7842b1392561af4b9547245550c410bb890
SpaGettys <jgettys@gmail.com>
2022-05-08 14:22:29 +0900

    Post changes of Activities to the API Server (Create/Edit)

--- Reactivities/client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ Reactivities/client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -15,9 +15,10 @@ interface Props {
   closeForm: () => void;
   createOrEdit: (activity: Activity) => void;
   deleteActivity: (id: string) => void;
+  submitting: boolean;
 }
 
-export default function ActivityDashboard({ activities, selectedActivity, selectActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, deleteActivity }: Props) {
+export default function ActivityDashboard({ activities, selectedActivity, selectActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, deleteActivity, submitting }: Props) {
   return (
     <Grid>
       <Grid.Column width='10'>
@@ -25,7 +26,7 @@ export default function ActivityDashboard({ activities, selectedActivity, select
       </Grid.Column>
       <Grid.Column width='6'>
         {selectedActivity && !editMode && <ActivityDetails activity={selectedActivity} cancelSelectActivity={cancelSelectActivity} openForm={openForm} />}
-        {editMode && <ActivityForm selectedActivity={selectedActivity} closeForm={closeForm} createOrEdit={createOrEdit} />}
+        {editMode && <ActivityForm selectedActivity={selectedActivity} closeForm={closeForm} createOrEdit={createOrEdit} submitting={submitting} />}
       </Grid.Column>
     </Grid>
   );
