30f52a366acdd392e65512185d4a1627cef072fa
SpaGettys <jgettys@gmail.com>
2022-05-08 21:32:52 +0900

    Observe the title property from the App Component

--- Reactivities/client-app/src/app/models/activityStores.ts
+++ Reactivities/client-app/src/app/models/activityStores.ts
@@ -1,9 +1,13 @@
-import { makeObservable, observable } from 'mobx';
+import { action, makeObservable, observable } from 'mobx';
 
 export default class ActivityStore {
   title = 'Hello from MobX!';
 
   constructor() {
-    makeObservable(this, { title: observable });
+    makeObservable(this, { title: observable, setTitle: action });
   }
+
+  setTitle = () => {
+    this.title = this.title + '!';
+  };
 }
