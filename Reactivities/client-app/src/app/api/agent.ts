1b9cdc75428f5427df80bdb843407830e8182595
SpaGettys <jgettys@gmail.com>
2022-05-16 18:50:40 +0900

    Add a key to the route so we can manually reload/reset it
    Fix a NavBar link as well

--- Reactivities/client-app/src/app/api/agent.ts
+++ Reactivities/client-app/src/app/api/agent.ts
@@ -23,7 +23,10 @@ const responseBody = <T>(response: AxiosResponse<T>) => response.data;
 
 const requests = {
   get: <T>(url: string) => axios.get<T>(url).then(responseBody),
-  post: <T>(url: string, body: {}) => axios.post<T>(url, body).then(responseBody),
+  post: <T>(url: string, body: {}) => {
+    console.log('post body: ' + body);
+    axios.post<T>(url, body).then(responseBody);
+  },
   put: <T>(url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
   del: <T>(url: string) => axios.delete<T>(url).then(responseBody),
 };
