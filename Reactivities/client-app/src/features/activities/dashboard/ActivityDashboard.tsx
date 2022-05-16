d6a42f88e0c5ce29b862e324e349b773c92def77
SpaGettys <jgettys@gmail.com>
2022-05-16 16:50:16 +0900

    Add some Routes to our app

--- Reactivities/client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ Reactivities/client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Grid } from 'semantic-ui-react';
 import ActivityDetails from '../details/ActivityDetails';
 import ActivityList from './ActivityList';
 import ActivityForm from './../form/ActivityForm';
 import { useStore } from '../../../app/models/store';
 import { observer } from 'mobx-react-lite';
+import LoadingComponent from '../../../app/layout/LoadingComponent';
 
 export default observer(function ActivityDashboard() {
   const { activityStore } = useStore();
   const { selectedActivity, editMode } = activityStore;
+
+  useEffect(() => {
+    activityStore.loadActivities();
+  }, [activityStore]);
+
+  if (activityStore.loadingInitial) return <LoadingComponent content='Loading app' />;
 
   return (
     <Grid>
