d67f14b6cb7c1daf3eab7db75d6d1e5cc64210df
SpaGettys <jgettys@gmail.com>
2022-06-02 21:57:50 +0900

    Hook up the activity form submission to Formik

--- Reactivities/client-app/src/features/activities/form/ActivityForm.tsx
+++ Reactivities/client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,11 +1,11 @@
 import { observer } from 'mobx-react-lite';
-import { ChangeEvent, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link, useHistory, useParams } from 'react-router-dom';
-import { Button, Segment } from 'semantic-ui-react';
+import { Button, Header, Segment } from 'semantic-ui-react';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
 import { useStore } from '../../../app/stores/store';
 import { v4 as uuid } from 'uuid';
-import { Formik, Form, ErrorMessage } from 'formik';
+import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
 import MyTextInput from '../../../app/common/form/MyTextInput';
 import MySelectInput from './../../../app/common/form/MySelectInput';
@@ -42,37 +42,34 @@ export default observer(function ActivityForm() {
     if (id) loadActivity(id).then((activity) => setActivity(activity!));
   }, [id, loadActivity]);
 
-  // function handleSubmit() {
-  //   if (activity.id.length === 0) {
-  //     let newActivity = {
-  //       ...activity,
-  //       id: uuid(),
-  //     };
-  //     createActivity(newActivity).then(() => history.push(`/activities/${newActivity.id}`));
-  //   } else {
-  //     updateActivity(activity).then(() => history.push(`/activities/${activity.id}`));
-  //   }
-  // }
-
-  // function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
-  //   const { name, value } = event.target;
-  //   setActivity({ ...activity, [name]: value });
-  // }
+  function handleFormSubmit(activity: Activity) {
+    if (activity.id.length === 0) {
+      let newActivity = {
+        ...activity,
+        id: uuid(),
+      };
+      createActivity(newActivity).then(() => history.push(`/activities/${newActivity.id}`));
+    } else {
+      updateActivity(activity).then(() => history.push(`/activities/${activity.id}`));
+    }
+  }
 
   if (loadingInitial) return <LoadingComponent content='Loading activity...' />;
 
   return (
     <Segment clearing>
-      <Formik validationSchema={validationSchema} enableReinitialize initialValues={activity} onSubmit={(values) => console.log(values)}>
-        {({ handleSubmit }) => (
+      <Header content='Activity Details' sub color='teal' />
+      <Formik validationSchema={validationSchema} enableReinitialize initialValues={activity} onSubmit={(values) => handleFormSubmit(values)}>
+        {({ handleSubmit, isValid, isSubmitting, dirty }) => (
           <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
             <MyTextInput placeholder='Title' name='title' />
             <MyTextInput placeholder='Description' name='description' />
             <MySelectInput options={categoryOptions} placeholder='Category' name='category' />
             <MyDateInput placeholderText='Date' name='date' showTimeSelect timeCaption='time' dateFormat='MMMM d, yyyy h:mm aa' />
+            <Header content='Location Details' sub color='teal' />
             <MyTextInput placeholder='City' name='city' />
             <MyTextInput placeholder='Venue' name='venue' />
-            <Button loading={loading} floated='right' positive type='submit' content='Submit' />
+            <Button disabled={isSubmitting || !dirty || !isValid} loading={loading} floated='right' positive type='submit' content='Submit' />
             <Button as={Link} to='/activities' floated='right' type='button' content='Cancel' />
           </Form>
         )}
