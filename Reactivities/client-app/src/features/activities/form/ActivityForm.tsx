0dbde8cfc677871157f84815d1fc5a95757d6964
SpaGettys <jgettys@gmail.com>
2022-06-02 19:40:26 +0900

    Create a reusable text input
    https://formik.org/docs/api/useField
    https://formik.org/docs/api/useField#fieldinputpropsvalue

--- Reactivities/client-app/src/features/activities/form/ActivityForm.tsx
+++ Reactivities/client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,12 +1,13 @@
 import { observer } from 'mobx-react-lite';
 import { ChangeEvent, useEffect, useState } from 'react';
 import { Link, useHistory, useParams } from 'react-router-dom';
-import { Button, FormField, Label, Segment } from 'semantic-ui-react';
+import { Button, Segment } from 'semantic-ui-react';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
 import { useStore } from '../../../app/stores/store';
 import { v4 as uuid } from 'uuid';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
+import MyTextInput from '../../../app/common/form/MyTextInput';
 
 export default observer(function ActivityForm() {
   const history = useHistory();
@@ -26,6 +27,11 @@ export default observer(function ActivityForm() {
 
   const validationSchema = Yup.object({
     title: Yup.string().required('The activity title is required'),
+    description: Yup.string().required('The activity description is required'),
+    category: Yup.string().required(),
+    date: Yup.string().required('Date is required').nullable(),
+    venue: Yup.string().required(),
+    city: Yup.string().required(),
   });
 
   useEffect(() => {
@@ -56,15 +62,12 @@ export default observer(function ActivityForm() {
       <Formik validationSchema={validationSchema} enableReinitialize initialValues={activity} onSubmit={(values) => console.log(values)}>
         {({ handleSubmit }) => (
           <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
-            <FormField>
-              <Field placeholder='Title' name='title' />
-              <ErrorMessage name='title' render={(error) => <Label basic color='red' content={error} />} />
-            </FormField>
-            <Field placeholder='Description' name='description' />
-            <Field placeholder='Category' name='category' />
-            <Field type='date' placeholder='Date' name='date' />
-            <Field placeholder='City' name='city' />
-            <Field placeholder='Venue' name='venue' />
+            <MyTextInput placeholder='Title' name='title' />
+            <MyTextInput placeholder='Description' name='description' />
+            <MyTextInput placeholder='Category' name='category' />
+            <MyTextInput placeholder='Date' name='date' />
+            <MyTextInput placeholder='City' name='city' />
+            <MyTextInput placeholder='Venue' name='venue' />
             <Button loading={loading} floated='right' positive type='submit' content='Submit' />
             <Button as={Link} to='/activities' floated='right' type='button' content='Cancel' />
           </Form>
