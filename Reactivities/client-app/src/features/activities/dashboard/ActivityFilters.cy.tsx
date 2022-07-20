531b5368462d911c418c3db79f6be17abef222fb
SpaGettys <jgettys@gmail.com>
2022-07-21 00:21:26 +0900

    Setup Cypress to be able to use Component test
    Note, that I'm still not able to import the sematic-ui styles for some reason...

---
+++ Reactivities/client-app/src/features/activities/dashboard/ActivityFilters.cy.tsx
@@ -0,0 +1,8 @@
+import ActivityFilters from './ActivityFilters';
+//import "../../../../node_modules/semantic-ui-css/semantic.min.css";
+
+describe('ActivityFilters.cy.tsx', () => {
+  it('playground', () => {
+    cy.mount(<ActivityFilters />)
+  })
+})
